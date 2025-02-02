84570af700a8de558daf00a3bbf41b683bd60d5c
beingaayush <beingaayushraj@gmail.com>
2025-02-02 21:50:15 +0530

    i've fixed the Reset button

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-//firtstly we're accessing those elements which we need further
+//firtstly we're accessing all elements which we need further
 let boxes = document.querySelectorAll(".box"); //here i used querySelectotAll coz multiple classes exist with same name.
-let resetbtn = document.querySelector("#reset-game");
 let newgamebtn = document.querySelector("#new-game");
+const resetbtn = document.getElementById("reset-game");
 let winalert = document.querySelector(".winalert");
 let winmsg = document.querySelector(".winmsg");
 
 
 
-let turnO = true;
+let turnO = true;    //means starting player will be "O"
 let count = 0; //to track draw
 
 let winPatterns = [   //these are the winning indices pattern
@@ -24,21 +24,31 @@ let winPatterns = [   //these are the winning indices pattern
 ];
 
 
+
+//newgame button code
 const newGame = () => {
     turnO = true;
     count = 0;
     enableBoxes();
     winalert.classList.add("hide");
 };
-
-// const resetGame = () => {
-//     turnO = true;
-//     count = 0;
-//     enableBoxes();
-//     winalert.classList.add("hide");
-// };
+newgamebtn.addEventListener("click", newGame);
 
 
+
+//reset button code
+const resetGame = () => {
+    boxes.forEach(box => {
+        box.textContent = "";  //clear the box content
+    });
+    turnO = true;     //it reset game variable , starts again with playerO
+    enableBoxes();     //this fnx will enable all boxes again
+};
+resetbtn.addEventListener("click", resetGame);    //
+
+
+
+//code for the cells(boxes)
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("button clicked");
@@ -52,20 +62,24 @@ boxes.forEach((box) => {
         }
         box.disabled = true;
         count++;
-        let isWinner = checkWinner();
-        // checkWinner(); //its our callBack fnx.
+        let isWinner = checkWinner();   // checkWinner(); //its our callBack fnx.     
         if (count === 9 && !isWinner) {
             gameDraw();
         }
     });
 });
 
+
+//fnx code after the game will draw
 const gameDraw = () => {
     winmsg.innerText = "GAME DRAW";
     winalert.classList.remove("hide");
     disableBoxes();
 };
 
+
+
+//this fnx will disable all boxes
 const disableBoxes = () => {
     for (let box of boxes) {
         box.disabled = true;
@@ -73,21 +87,29 @@ const disableBoxes = () => {
     }
 }
 
+
+//this fnx will enable all boxes
 const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false;
     }
 }
 
+
+
+//this fnx will be call when any of two player will won
 const showWinner = (Winner) => {
     winmsg.innerText = `Congratulation, Winner is ${Winner}`
     winalert.classList.remove("hide");
     disableBoxes();
 }
 
+
+
+//this fnx code will be use to check who is the winner
 const checkWinner = () => {
     for (let pattern of winPatterns) {
-        // console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
@@ -100,35 +122,29 @@ const checkWinner = () => {
     }
 };
 
-newgamebtn.addEventListener("click", newGame);
-// resetbtn.addEventListener("click", resetGame);
-resetbtn.addEventListener("click", () => {
-    turnO = true;
-    count = 0;
-    enableBoxes();
-    winalert.classList.add("hide");
-});
 
 
 
-//changing screen mode
+// code for changing screen mode 
 
 let modebtn = document.querySelector("#mode");
 let body = document.querySelector("body");
 
 let currMode = "light";
 
-modebtn.addEventListener("click",() => {
-if(currMode === "light"){
-    currMode = "dark";
-    body.classList.add("dark");
-    body.classList.remove("light");
-} else {
-    currMode = "light";
-    body.classList.add("light");
-    body.classList.remove("dark");
-}
-console.log(currMode);
+modebtn.addEventListener("click", () => {
+    if (currMode === "light") {
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        currMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(currMode);
 });
 
-//All SEt !! <3
+
+
+//All SEt !! :)
